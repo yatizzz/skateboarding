1d0dacef82c4c98b70f53b714a57eaa5d3967eeb
electroplita <37555079+yatizzz@users.noreply.github.com>
2021-06-03 22:28:05 +0300

    lab56

---
+++ myform.py
@@ -0,0 +1,16 @@
+import re
+from bottle import post, request
+
+@post('/home', method='post')
+
+def my_form():
+    mail = request.forms.get('ADRESS')
+    question = request.forms.get('QUEST')
+    if((request.forms.get('ADRESS')=='')|(request.forms.get('QUEST')=='')):
+        return "Please, put all information"
+    else:
+        if(re.match(r"^\w+@\w+(\.\w+)+$", mail)):
+            mail = request.forms.get('ADRESS')
+            return "Thanks! The answer will be sent to the mail %s" % mail
+        else:
+            return "Please, put a correct email."

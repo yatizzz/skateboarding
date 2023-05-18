ed0d4804417f87e6ee0fffa8b50cc52b8382469f
electroplita <yatizzz@yandex.ru>
2023-05-18 11:48:38 +0300

    добавление имени в форму

--- myform.py
+++ myform.py
@@ -6,11 +6,12 @@ from bottle import post, request
 def my_form():
     mail = request.forms.get('ADRESS')
     question = request.forms.get('QUEST')
+    username = request.forms.get('USERNAME')
     if((request.forms.get('ADRESS')=='')|(request.forms.get('QUEST')=='')):
         return "Please, put all information"
     else:
         if(re.match(r"^\w+@\w+(\.\w+)+$", mail)):
             mail = request.forms.get('ADRESS')
-            return "Thanks! The answer will be sent to the mail %s" % mail
+            return "Thanks, %s" % username+ "!The answer will be sent to the mail %s" % mail
         else:
             return "Please, put a correct email."
--- views/index.tpl
+++ views/index.tpl
@@ -37,6 +37,7 @@
         <form action="/home" method="post">
         <p><textarea rows="2" cols="50" name="QUEST" placeholder="Your question"></textarea></p> 
         <p><input type="text" size="50" name="ADRESS" placeholder="Your email"></p>
+        <p><input type="text" size="50" name="USERNAME" placeholder="Your name"></p>
         <p><input type="submit" value="Send"></p>
        
 </form>
